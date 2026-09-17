b20f0746edeab6af9cfa3cd9a63f65e15d164bb3
Aristeu2266 <mysteamaccountx@hotmail.com>
2026-09-16 21:18:52 -0300

    tratamente de erros

--- zenon-fraud-detector/src/main/java/br/com/zenon/TransactionCustomer.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionCustomer.java
@@ -1,10 +1,19 @@
 package br.com.zenon;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record TransactionCustomer(String name, BigDecimal oldBalance, BigDecimal newBalance) {
-    public TransactionCustomer(String name, double oldBalance, double newBalance) {
-        this(name, BigDecimal.valueOf(oldBalance), BigDecimal.valueOf(newBalance));
+    public TransactionCustomer {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(oldBalance);
+        Objects.requireNonNull(newBalance);
+
+        if (name.isEmpty()) throw new IllegalArgumentException("name should not be empty");
+        if (oldBalance.signum() < 0)
+            throw new IllegalArgumentException("oldBalance should be positive: " + oldBalance);
+        if (newBalance.signum() < 0)
+            throw new IllegalArgumentException("newBalance should be positive: " + newBalance);
     }
 
     @Override
--- zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
@@ -6,14 +6,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class TransactionIngestor {
     private final String path;
     private List<Transaction> transactions;
     private int numberOfLines;
 
+    private final Logger logger = Logger.getLogger("TransactionIngestor");
+
     public TransactionIngestor(String path) {
         this.path = path;
+        this.numberOfLines = 0;
         loadTransactions();
     }
 
@@ -27,51 +32,56 @@ public class TransactionIngestor {
         Path path = Path.of(this.path);
         transactions = new ArrayList<>();
 
-        try(BufferedReader br = Files.newBufferedReader(path)) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
 
             br.readLine();
 
-            if (numberOfLines > 0) {
-                for (int i = 0; i < numberOfLines; i++) {
-                    line = br.readLine();
-                    String[] fields = line.split(",");
-
-                    transactions.add(parseTransaction(fields));
-                }
-            } else {
-                while((line = br.readLine()) != null){
-                    String[] fields = line.split(",");
-
-                    transactions.add(parseTransaction(fields));
-                }
+            int lineCount = 0;
+            while (((line = br.readLine()) != null) && ((numberOfLines == 0) || (lineCount++ < numberOfLines))) {
+                parseTransaction(line).ifPresent(transactions::add);
             }
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("File could not be read:\n" + e);
         }
     }
 
-    private static Transaction parseTransaction(String[] fields) {
-        int step = Integer.parseInt(fields[0]);
-        TransactionType type = TransactionType.valueOf(fields[1]);
-        BigDecimal amount = new BigDecimal(fields[2]);
-        String nameOrig = fields[3];
-        BigDecimal oldBalanceOrg = new BigDecimal(fields[4]);
-        BigDecimal newBalanceOrig = new BigDecimal(fields[5]);
-        String nameDest = fields[6];
-        BigDecimal oldBalanceDest = new BigDecimal(fields[7]);
-        BigDecimal newBalanceDest = new BigDecimal(fields[8]);
-        boolean isFraud = "1".equals(fields[9]);
-        boolean isFlaggedFraud = "1".equals(fields[10]);
+    private Optional<Transaction> parseTransaction(String line) {
+        try {
+            String[] fields = line.split(",");
 
-        var customer = new TransactionCustomer(nameOrig, oldBalanceOrg, newBalanceOrig);
-        var recipient  = new TransactionCustomer(nameDest, oldBalanceDest, newBalanceDest);
+            int step = Integer.parseInt(fields[0]);
+            TransactionType type = TransactionType.valueOf(fields[1]);
+            BigDecimal amount = new BigDecimal(fields[2]);
+            String nameOrig = fields[3];
+            BigDecimal oldBalanceOrigin = new BigDecimal(fields[4]);
+            BigDecimal newBalanceOrigin = new BigDecimal(fields[5]);
+            String nameDest = fields[6];
+            BigDecimal oldBalanceDest = new BigDecimal(fields[7]);
+            BigDecimal newBalanceDest = new BigDecimal(fields[8]);
+            boolean isFraud = "1".equals(fields[9]);
+            boolean isFlaggedFraud = "1".equals(fields[10]);
 
-        return new Transaction(step, type, amount, customer, recipient, isFraud, isFlaggedFraud);
+            var customer = new TransactionCustomer(nameOrig, oldBalanceOrigin, newBalanceOrigin);
+            var recipient = new TransactionCustomer(nameDest, oldBalanceDest, newBalanceDest);
+
+            return Optional.of(new Transaction(step, type, amount, customer, recipient, isFraud, isFlaggedFraud));
+        } catch (Exception e) {
+            this.logger.severe(() -> line + " | " + e);
+            return Optional.empty();
+        }
     }
 
     public List<Transaction> getTransactions() {
         return transactions;
+    }
+
+    public int getNumberOfLines() {
+        return numberOfLines;
+    }
+
+    public void setNumberOfLines(int numberOfLines) {
+        this.numberOfLines = numberOfLines;
     }
 }
