32bf4ffcea8dd47cc8da6cfa3624d50f9488bc22
Aristeu2266 <mysteamaccountx@hotmail.com>
2026-09-21 14:10:43 -0300

    benchmark list vs map

--- zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -1,24 +1,32 @@
 package br.com.zenon;
 
-import java.util.List;
-
 public class Main {
     void main(String[] args) {
-        TransactionIngestor ingestor = new TransactionIngestor("data/PS_20174392719_1491204439457_log.csv", 50000);
+        TransactionIngestor ingestor = new TransactionIngestor("data/PS_20174392719_1491204439457_log.csv", 100000);
 
-        List<Transaction> frauds = FraudAnalyzer.filterFrauds(ingestor.getTransactions());
-        IO.println("1. Total de Fraudes: " + frauds.size());
+        TransactionRepository transactionRepository = new TransactionListRepository(ingestor.getTransactions());
 
-        IO.println("2. Top 3 Fraudes de Maior Valor:");
-        FraudAnalyzer.highestAmountsTransactions(frauds, 3).forEach(t -> IO.println(t.amount().setScale(2).toPlainString()));
+        transactionRepository.getTransactionByOriginName("C12345")
+                .ifPresentOrElse(IO::println, () -> IO.println("Customer not found"));
+        transactionRepository.getTransactionByOriginName("C1231006815")
+                .ifPresentOrElse(IO::println, () -> IO.println("Customer not found"));
 
-        IO.println("3. Clientes Suspeitos:");
-        FraudAnalyzer.topSuspects(frauds, 5).forEach(IO::println);
+        var before = System.nanoTime();
 
-        IO.println("4. Prejuízo Total: " + FraudAnalyzer.totalAmount(frauds).toPlainString());
+        transactionRepository.getTransactionByOriginName("C1868032458")
+                .ifPresentOrElse(IO::println, () -> IO.println("Customer not found"));
 
-        IO.println("5. Fraudes por Tipo:");
-        FraudAnalyzer.countFraudsByType(frauds).forEach((k, v) -> IO.println(" - " + k + ": " + v));
+        var after = System.nanoTime();
+        IO.println("Time: " + (after - before) / 1000000.0 + "ms");
 
+        transactionRepository = new TransactionMapRepository(ingestor.getTransactions());
+
+        before = System.nanoTime();
+
+        transactionRepository.getTransactionByOriginName("C1868032458")
+                .ifPresentOrElse(IO::println, () -> IO.println("Customer not found"));
+
+        after = System.nanoTime();
+        IO.println("Time: " + (after - before) / 1000000.0 + "ms");
     }
 }
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionMapRepository.java
@@ -0,0 +1,24 @@
+package br.com.zenon;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TransactionMapRepository implements TransactionRepository{
+
+    private final Map<String, Transaction> transactionByOriginName;
+
+    public TransactionMapRepository(List<Transaction> transactions) {
+        Objects.requireNonNull(transactions);
+        this.transactionByOriginName = transactions.stream()
+                .collect(Collectors.toMap(t -> t.origin().name(), Function.identity()));
+    }
+
+    @Override
+    public Optional<Transaction> getTransactionByOriginName(String origin) {
+        return Optional.ofNullable(transactionByOriginName.get(origin));
+    }
+}
